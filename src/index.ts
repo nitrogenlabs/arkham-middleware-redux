ace10cdcd9f49ac8498fd048d68a7c596c1574ce
Giraldo Rosales <giraldo@nitrogenlabs.com>
2018-06-21 18:16:20 -0500

    Clean up

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,9 @@
  * Copyright (c) 2018-Present, Nitrogen Labs, Inc.
  * Copyrights licensed under the MIT License. See the accompanying LICENSE file for terms.
  */
+import {createArkhamStore} from './createArkhamStore';
+import {arkhamMiddleware} from './middleware/arkhamMiddleware';
+import {ReduxMiddleware} from './middleware/ReduxMiddleware';
+import {ArkhamReduxStoreType} from './types/main';
 
-// Middleware for Redux
-export {arkhamMiddleware} from './middleware/arkhamMiddleware';
-
-// Create a Redux store which integrates with ArkhamJS
-export {createArkhamStore} from './createArkhamStore';
-
-// Middleware for ArkhamJS
-export {ReduxMiddleware} from './middleware/ReduxMiddleware';
-
-// Types
-export {ArkhamReduxStoreType} from './types/main';
+export {arkhamMiddleware, ArkhamReduxStoreType, createArkhamStore, ReduxMiddleware};
